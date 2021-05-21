24e8be43a03c20a4612736048e4f60ea6b9232b6
Robert Ransom <rransom.8774@gmail.com>
2021-05-21 16:45:52 -0400

    Modify randombytes shim to call NIST known-answer test DRBG

--- src/pkpsig/randombytes.c
+++ src/pkpsig/randombytes.c
@@ -10,11 +10,12 @@
 
 #include "randombytes.h"
 
-/* prototype copied from "../ssh.h" */
-void random_read(void *out, size_t size);
+/* copied from NIST rng.h for test purposes */
+int
+randombytes(unsigned char *x, unsigned long long xlen);
 
 int pkpsig_randombytes(uint8_t *out, size_t len) {
-    random_read(out, len);
+    randombytes(out, len);
     return 0;
 };
 
