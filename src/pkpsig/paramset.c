5440614b6fb1de5030f30c9f82da4f0cfc6019eb
Robert Ransom <rransom.8774@gmail.com>
2021-06-21 09:24:51 -0400

    Reorder security levels in roughly increasing order

--- src/pkpsig/paramset.c
+++ src/pkpsig/paramset.c
@@ -30,17 +30,17 @@ static size_t compute_treehash_degree(struct pkpsig_paramset *ps) {
 };
 
 static const struct pkpsig_seclevel seclevels[] =
-  { { "c1",  16, 32 },
+  { { "b80",     10, 20},
+    { "b96",     12, 24},
+    { "b112git", 14, 20},
+    { "b112",    14, 28},
+    { "c1",  16, 32 },
     { "c1a", 20, 32 },
     { "c2",  24, 32 },
     { "c3",  24, 48 },
     { "c4",  32, 48 },
     { "c5",  32, 64 },
     { "c6",  48, 64 }, /* may be more conservative than other definitions */
-    { "b112git", 14, 20},
-    { "b112",    14, 28},
-    { "b96",     12, 24},
-    { "b80",     10, 20},
     { NULL, 0, 0 }
   };
 
